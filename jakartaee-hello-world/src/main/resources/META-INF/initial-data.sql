eed900f509cf0e81d77f084bfbe501fb5eff2598
jacqeinz <jacquicv.19@gmail.com>
2024-04-02 11:14:57 -0600

    corrections to initial data

--- jakartaee-hello-world/src/main/resources/META-INF/initial-data.sql
+++ jakartaee-hello-world/src/main/resources/META-INF/initial-data.sql
@@ -1,6 +1,7 @@
-INSERT INTO account (firstName, lastName, homeAddress, dateOfBirth, email, phoneNumber, userName, userPassword) VALUES ('Jacqueline', 'Chavez', '111 Roadway', '5/9/1996', 'user@gmail.com', '461-8786', 'user', 'password');
-INSERT INTO room (roomNumber, price, floorNumber) VALUES ('Room 128', 199, 1);
+INSERT INTO account (Name, homeAddress, dateOfBirth, email, phoneNumber, userName, userPassword) VALUES ('Jacqueline Chavez', '111 Roadway', '5/9/1996', 'user@gmail.com', '461-8786', 'user', 'password');
 
-INSERT INTO reservationDetail (guest) VALUES ('Jacqueline');
-INSERT INTO roomReservation (guest) VALUES ('Jacqueline');
-INSERT INTO reservation (guest) VALUES ('Jacqueline');
+INSERT INTO room (roomNumber, roomType, price, floorNumber, bookedDates) VALUES ('Room 128', 'Queen', 199, 1, [1/1/2034]);
+
+INSERT INTO reservationDetails (bookedList, guest) VALUES ([01/20/2016, Jacqueline Chavez);
+INSERT INTO roomReservation (guest, room, dates) VALUES (Jacqueline Chavez, Room 128, 299, '01/01/2056', '01/10/2056');
+INSERT INTO reservation (guest, roomReservation, paymentInformation, occupants) VALUES ('Jacqueline Chavez');
