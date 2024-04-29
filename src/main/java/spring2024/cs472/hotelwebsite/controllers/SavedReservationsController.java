30a4d2a6947f8c17e519eb387ce0339270de5e0a
Benaiah <benaiah.gingrich@gmail.com>
2024-04-28 23:52:34 -0600

    Room Reservation Report for Admin created and formatted (SavedReservationsReport). Added a method to ReservationDetails to support the Room Reservation Report.

---
+++ src/main/java/spring2024/cs472/hotelwebsite/controllers/SavedReservationsController.java
@@ -0,0 +1,35 @@
+package spring2024.cs472.hotelwebsite.controllers;
+
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import spring2024.cs472.hotelwebsite.entities.ReservationDetails;
+import spring2024.cs472.hotelwebsite.entities.Room;
+import spring2024.cs472.hotelwebsite.repositories.ReservationDetailsRepository;
+import spring2024.cs472.hotelwebsite.repositories.RoomRepository;
+import spring2024.cs472.hotelwebsite.repositories.RoomReservationRepository;
+import spring2024.cs472.hotelwebsite.services.RoomService;
+
+@Controller
+public class SavedReservationsController {
+
+    private final ReservationDetailsRepository reservationDetailsRepository;
+
+    public SavedReservationsController(ReservationDetailsRepository reservationDetailsRepository) {
+
+        this.reservationDetailsRepository = reservationDetailsRepository;
+    }
+
+    @GetMapping("/SavedReservationReport")
+    public String showReservationList(Model model, HttpSession session){
+//        if(session.getAttribute("admin") == null) {
+//            return "redirect:/login";
+//        }
+        model.addAttribute("SavedReservations", reservationDetailsRepository.findAll());
+        return "SavedReservationReport";
+    }
+
+}
--- src/main/java/spring2024/cs472/hotelwebsite/entities/GraphicalView.java
+++ src/main/java/spring2024/cs472/hotelwebsite/entities/GraphicalView.java
@@ -58,4 +58,3 @@ public class GraphicalView {
         return false;
     }
 }
-
