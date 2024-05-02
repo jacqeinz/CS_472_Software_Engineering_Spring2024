3315d823047eedb0e64c31473da9685ca4cfa852
jacqeinz <jacquicv.19@gmail.com>
2024-05-02 17:55:15 -0600

    Added Account Service Test

--- src/test/java/JUnitTests.java
+++ src/test/java/JUnitTests.java
@@ -1,10 +1,38 @@
+
+import static org.apache.coyote.http11.Constants.a;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.springframework.validation.BindingResultUtils.getBindingResult;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+
 
 import java.beans.PropertyEditor;
 import java.util.*;
+import java.util.function.Function;
 
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.springframework.beans.PropertyEditorRegistry;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.repository.query.FluentQuery;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
@@ -13,96 +41,47 @@ import spring2024.cs472.hotelwebsite.controllers.*;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import spring2024.cs472.hotelwebsite.*;
+import spring2024.cs472.hotelwebsite.entities.Account;
 import spring2024.cs472.hotelwebsite.entities.Admin;
+import spring2024.cs472.hotelwebsite.entities.Guest;
+import spring2024.cs472.hotelwebsite.repositories.AccountRepository;
 import spring2024.cs472.hotelwebsite.services.AccountService;
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+@SpringBootTest(
+        webEnvironment = SpringBootTest.WebEnvironment.MOCK,
+        classes = HotelWebsiteApplication.class)
+@AutoConfigureMockMvc
+class JUnitTests {
 
-public class JUnitTests {
+    @Autowired
+    private AccountService accountService;
+
+    @MockBean
+    private AccountRepository accountRepository;
+
+//    @Before
+//    public void setUp() {
+//    }
+
+//    @Test
+//    void showUpdateFormGuest() {
+//    }
+
     @Test
+    public void validateLoginShouldReturnNull() {
+        Guest guest = new Guest("Guest Guesterson", "123 Guest St", "1/2/3456", "guest@guest.guest" ,"123-456-7890", "guest",
+                "badPassword1", "1234567876543345678");
+        guest.setId(1L);
+        List<Account> accounts = List.of(guest);
+        when(accountRepository.findAll()).thenReturn(accounts);
 
-    public void testAdminEdit() {
-        AccountService accountService = new AccountService();
-        AdminAccountCrudController adminAccountCrudController = new AdminAccountCrudController(accountService);
-        adminAccountCrudController.updateAdmin(1, accountService.getAllAdmins().get(1), Objects.requireNonNull(getBindingResult()));
+        Account account = accountService.validateLogin("guest", "wrongPassword2");
 
+        assertNull(account);
 
     }
 
-    private BindingResult getBindingResult() {
-        return new BindingResult() {
-            @Override
-            public String getObjectName() {
-                return "";
-            }
 
-            @Override
-            public void reject(String errorCode, Object[] errorArgs, String defaultMessage) {
-
-            }
-
-            @Override
-            public void rejectValue(String field, String errorCode, Object[] errorArgs, String defaultMessage) {
-
-            }
-
-            @Override
-            public List<ObjectError> getGlobalErrors() {
-                return List.of();
-            }
-
-            @Override
-            public List<FieldError> getFieldErrors() {
-                return List.of();
-            }
-
-            @Override
-            public Object getFieldValue(String field) {
-                return null;
-            }
-
-            @Override
-            public String toString() {
-                return "";
-            }
-
-            @Override
-            public Object getTarget() {
-                return null;
-            }
-
-            @Override
-            public Map<String, Object> getModel() {
-                return Map.of();
-            }
-
-            @Override
-            public Object getRawFieldValue(String field) {
-                return null;
-            }
-
-            @Override
-            public PropertyEditor findEditor(String field, Class<?> valueType) {
-                return null;
-            }
-
-            @Override
-            public PropertyEditorRegistry getPropertyEditorRegistry() {
-                return null;
-            }
-
-            @Override
-            public String[] resolveMessageCodes(String errorCode) {
-                return new String[0];
-            }
-
-            @Override
-            public String[] resolveMessageCodes(String errorCode, String field) {
-                return new String[0];
-            }
-
-            @Override
-            public void addError(ObjectError error) {
-
-            }
-        };
-    }
 }
+
