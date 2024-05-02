70946e6eb4b98d452e78d0ad500ff054c64066a9
jacqeinz <jacquicv.19@gmail.com>
2024-05-01 20:08:50 -0600

    removed jakarta file, added junit dependency and j unit test file

--- src/main/java/spring2024/cs472/hotelwebsite/controllers/GuestAccountCrudController.java
+++ src/main/java/spring2024/cs472/hotelwebsite/controllers/GuestAccountCrudController.java
@@ -7,6 +7,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import spring2024.cs472.hotelwebsite.entities.Guest;
 import spring2024.cs472.hotelwebsite.repositories.AccountRepository;
