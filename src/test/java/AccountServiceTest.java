0bff03110e5594726b645beefe2cb35c3ae8c270
Devon <alonzod@enmu.edu>
2024-05-05 12:41:43 -0600

    check out page updated

--- src/test/java/AccountServiceTest.java
+++ src/test/java/AccountServiceTest.java
@@ -1,8 +1,10 @@
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 
+import java.time.LocalDateTime;
 import java.util.*;
 
 import org.junit.runner.RunWith;
@@ -18,7 +20,12 @@ import spring2024.cs472.hotelwebsite.entities.Account;
 import spring2024.cs472.hotelwebsite.entities.Admin;
 import spring2024.cs472.hotelwebsite.entities.Guest;
 import spring2024.cs472.hotelwebsite.repositories.AccountRepository;
+import spring2024.cs472.hotelwebsite.repositories.TokenRepository;
 import spring2024.cs472.hotelwebsite.services.AccountService;
+import spring2024.cs472.hotelwebsite.services.ResetPasswordService;
+import org.springframework.ui.Model;
+import spring2024.cs472.hotelwebsite.entities.PasswordResetToken;
+
 @RunWith(SpringJUnit4ClassRunner.class)
 @WebAppConfiguration
 @SpringBootTest(
@@ -30,8 +37,17 @@ class AccountServiceTest {
     @Autowired
     private AccountService accountService;
 
+    @Autowired
+    private ResetPasswordService resetPasswordService;
+
     @MockBean
     private AccountRepository accountRepository;
+
+    @MockBean
+    private Model model;
+    @Autowired
+    private TokenRepository tokenRepository;
+
 
 //    @Before
 //    public void setUp() {
@@ -74,7 +90,7 @@ class AccountServiceTest {
         admin.setId(1L);
         List<Account> accounts = List.of(admin);
         when(accountRepository.findAll()).thenReturn(accounts);
-        Account account = accountService.validateLogin("admin", "admin");
+        Account account = accountService.validateLogin("Admin", "admin");
         assertNotNull(account);
     }
 
@@ -89,6 +105,59 @@ class AccountServiceTest {
         Account account = accountService.validateLogin("admin", "incorrectPassword1");
         assertNull(account);
     }
+
+    @Test
+    public void resetPasswordEmailIsSent(){
+        Account account = new Account("Guest Guesterson", "123 Guest St", "1/2/3456", "guest@guest.guest" ,"123-456-7890",
+                "user", "12");
+        assertEquals("Success", accountService.sendEmail(account));
+
+    }
+
+    @Test
+    public void resetTokenCreatedSuccessfully(){
+        Account account = new Account("Guest Guesterson", "123 Guest St", "1/2/3456", "guest@guest.guest" ,"123-456-7890", "guest",
+                "badPassword1", "1234567876543345678");
+        String result = accountService.generateResetToken(account);
+        assertNotNull(result);
+    }
+
+    @Test
+    public void accountPasswordChangedAfterReset(){
+        Account account = new Account("Guest Guesterson", "123 Guest St", "1/2/3456", "guest@guest.guest" ,"123-456-7890",
+                "user", "12");
+        accountRepository.save(account);
+        String result = accountService.generateResetToken(account);
+        when(accountRepository.findByEmail("guest@guest.guest")).thenReturn(account);
+        resetPasswordService.resetPasswordProcess(result, "1234", "guest@guest.guest");
+        assertEquals("1234", account.getUserPassword());
+    }
+
+    @Test void TokenIsExpired(){
+        Account account = new Account("Guest Guesterson", "123 Guest St", "1/2/3456", "guest@guest.guest" ,"123-456-7890",
+                "user", "12");
+        PasswordResetToken token = new PasswordResetToken();
+        LocalDateTime expiryDateTime = LocalDateTime.now().minusMinutes(2);
+        token.setExpiryDateTime(expiryDateTime);
+        assertFalse(accountService.isNotExpired(token.getExpiryDateTime()));
+
+    }
+    @Test void TokenIsNotExpired() {
+        Account account = new Account("Guest Guesterson", "123 Guest St", "1/2/3456", "guest@guest.guest", "123-456-7890",
+                "user", "12");
+        PasswordResetToken token = new PasswordResetToken();
+        LocalDateTime expiryDateTime = LocalDateTime.now().plusMinutes(2);
+        token.setExpiryDateTime(expiryDateTime);
+        assertTrue(accountService.isNotExpired(token.getExpiryDateTime()));
+    }
+
+    @Test void getAllAdminsRetrievesAllAdmins(){
+        Admin admin = new Admin(true, 123456);
+        List<Account> accounts = List.of(admin);
+        when(accountRepository.findAll()).thenReturn(accounts);
+        assertNotNull(accountService.getAllAdmins());
+    }
+
 
 
 }
