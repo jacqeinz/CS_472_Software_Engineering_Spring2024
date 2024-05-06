263f312e63143260b60caa4a9c00c9fb7853914f
Brawun <brandon.figueroa233295@potros.itson.edu.mx>
2024-05-06 10:37:10 -0600

    Class TokenRepository documented.

--- src/main/java/spring2024/cs472/hotelwebsite/repositories/TokenRepository.java
+++ src/main/java/spring2024/cs472/hotelwebsite/repositories/TokenRepository.java
@@ -1,10 +1,26 @@
+/**
+ * TokenRepository.java
+ */
 package spring2024.cs472.hotelwebsite.repositories;
 
+// Imports necessary for the class
 import org.springframework.data.jpa.repository.JpaRepository;
 import spring2024.cs472.hotelwebsite.entities.PasswordResetToken;
 
+/**
+ * Repository interface for managing password reset tokens.
+ * This interface defines methods for managing password reset tokens using JpaRepository.
+ * It provides a method for finding a password reset token by its token string.
+ *
+ * @author Team ABCFG
+ */
 public interface TokenRepository extends JpaRepository<PasswordResetToken, Integer>{
 
+    /**
+     * Finds a password reset token by its token string.
+     *
+     * @param token The token string.
+     * @return The password reset token with the specified token string, or null if not found.
+     */
     PasswordResetToken findByToken(String token);
-
 }
