4ae4bf5326c1af94bafdf11d6d3a1ff87034bfc5
Brawun <brandon.figueroa233295@potros.itson.edu.mx>
2024-05-06 08:37:59 -0600

    Class SavedReservationsController documented.

--- src/main/java/spring2024/cs472/hotelwebsite/controllers/SavedReservationsController.java
+++ src/main/java/spring2024/cs472/hotelwebsite/controllers/SavedReservationsController.java
@@ -1,5 +1,9 @@
+/**
+ * SavedReservationsController.java
+ */
 package spring2024.cs472.hotelwebsite.controllers;
 
+// Imports necessary for the class
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -13,16 +17,33 @@ import spring2024.cs472.hotelwebsite.repositories.RoomRepository;
 import spring2024.cs472.hotelwebsite.repositories.RoomReservationRepository;
 import spring2024.cs472.hotelwebsite.services.RoomService;
 
+/**
+ * This class is responsible for managing saved reservations.
+ *
+ * @author Team ABCFG
+ */
 @Controller
 public class SavedReservationsController {
 
+    // Attributes
     private final ReservationDetailsRepository reservationDetailsRepository;
 
+    /**
+     * Constructs a SavedReservationsController with the provided ReservationDetailsRepository.
+     *
+     * @param reservationDetailsRepository The repository for reservation details.
+     */
     public SavedReservationsController(ReservationDetailsRepository reservationDetailsRepository) {
-
         this.reservationDetailsRepository = reservationDetailsRepository;
     }
 
+    /**
+     * Displays a list of saved reservations.
+     *
+     * @param model   The model to which attributes are added.
+     * @param session The HTTP session.
+     * @return The view for displaying saved reservations.
+     */
     @GetMapping("/SavedReservationReport")
     public String showReservationList(Model model, HttpSession session){
 //        if(session.getAttribute("admin") == null) {
@@ -31,5 +52,4 @@ public class SavedReservationsController {
         model.addAttribute("SavedReservations", reservationDetailsRepository.findAll());
         return "SavedReservationReport";
     }
-
 }
