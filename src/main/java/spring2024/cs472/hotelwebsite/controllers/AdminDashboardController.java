e057fca05da5e32432ee09062f9468a7f59b496f
Brawun <brandon.figueroa233295@potros.itson.edu.mx>
2024-05-06 00:26:53 -0600

    Class AdminDashboardController documented.

--- src/main/java/spring2024/cs472/hotelwebsite/controllers/AdminDashboardController.java
+++ src/main/java/spring2024/cs472/hotelwebsite/controllers/AdminDashboardController.java
@@ -1,13 +1,30 @@
+/**
+ * AdminDashboardController.java
+ */
 package spring2024.cs472.hotelwebsite.controllers;
 
+// Imports necessary for the class
 import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+/**
+ * The AdminDashboardController class manages requests related to the admin dashboard and logout functionality.
+ *
+ * @author Team ABCFG
+ */
 @Controller
 public class AdminDashboardController {
+
+    /**
+     * Handles requests to display the admin dashboard.
+     *
+     * @param model The Model object to add attributes for the view.
+     * @param session The HttpSession object to check the admin session.
+     * @return The view name for the admin dashboard or a redirection to the login page if not logged in.
+     */
     @GetMapping("/adminDashboard")
     public String adminDashboard(Model model, HttpSession session) {
         if(session.getAttribute("admin") == null) {
@@ -17,10 +34,16 @@ public class AdminDashboardController {
         return "adminDashboard";
     }
 
+    /**
+     * Handles requests to log out the admin.
+     *
+     * @param model The Model object to add attributes for the view.
+     * @param session The HttpSession object to invalidate the admin session.
+     * @return The view name for logging out the admin.
+     */
     @GetMapping("/logoutAdmin")
     public String logoutAdmin(Model model, HttpSession session) {
         session.invalidate();
         return "logoutAdmin";
     }
-
 }
