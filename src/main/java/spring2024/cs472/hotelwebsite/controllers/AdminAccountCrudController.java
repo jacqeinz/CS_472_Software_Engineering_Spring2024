516083b687d71f6f3987003e96534d4b126ca3d2
Brawun <brandon.figueroa233295@potros.itson.edu.mx>
2024-05-06 00:22:47 -0600

    Class AdminAccountCrudController documented.

--- src/main/java/spring2024/cs472/hotelwebsite/controllers/AdminAccountCrudController.java
+++ src/main/java/spring2024/cs472/hotelwebsite/controllers/AdminAccountCrudController.java
@@ -1,5 +1,9 @@
+/**
+ * AdminAccountCrudController.java
+ */
 package spring2024.cs472.hotelwebsite.controllers;
 
+// Imports necessary for the class
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,20 +14,47 @@ import org.springframework.web.bind.annotation.PostMapping;
 import spring2024.cs472.hotelwebsite.entities.Admin;
 import spring2024.cs472.hotelwebsite.repositories.AccountRepository;
 import spring2024.cs472.hotelwebsite.services.AccountService;
+
+/**
+ * This class named AdminAccountCrudController is responsible for handling CRUD operations
+ * related to Admin accounts.
+ *
+ * @author Team ABCFG
+ */
 @Controller
 public class AdminAccountCrudController {
+
+    // Attributes
     @Autowired
     private AccountRepository accountRepository;
     private AccountService accountService;
 
-
+    /**
+     * Constructor for AdminAccountCrudController class.
+     *
+     * @param accountService The AccountService to be used.
+     */
     public AdminAccountCrudController(AccountService accountService) {this.accountService = accountService;}
 
+    /**
+     * This method retrieves all Admin accounts and displays them on the accountIndex page.
+     *
+     * @param model The Model object to add attributes for the view.
+     * @return The view name for displaying Admin accounts.
+     */
     @GetMapping("/AccountIndex")
     public String showUserList(Model model) {
         model.addAttribute("Admins", accountService.getAllAdmins());
         return "accountIndex";
     }
+
+    /**
+     * This method retrieves an Admin account for editing and displays the updateAdmin form.
+     *
+     * @param id The ID of the Admin account to be edited.
+     * @param model The Model object to add attributes for the view.
+     * @return The view name for updating an Admin account.
+     */
     @GetMapping("/admin/edit/{id}")
     public String showUpdateForm(@PathVariable("id") long id, Model model) {
         Admin admin = (Admin) accountRepository.findById(id);
@@ -32,6 +63,15 @@ public class AdminAccountCrudController {
         model.addAttribute("admin", admin);
         return "updateAdmin";
     }
+
+    /**
+     * This method updates an Admin account.
+     *
+     * @param id The ID of the Admin account to be updated.
+     * @param admin The updated Admin object.
+     * @param result The BindingResult object for validation.
+     * @return The redirect view name after updating the Admin account.
+     */
     @PostMapping("/admin/edit/{id}")
     public String updateAdmin(@PathVariable("id") long id, Admin admin,
                               BindingResult result) {
@@ -42,6 +82,14 @@ public class AdminAccountCrudController {
         accountRepository.save(admin);
         return "redirect:/AccountIndex";
     }
+
+    /**
+     * This method deletes an Admin account.
+     *
+     * @param id The ID of the Admin account to be deleted.
+     * @param model The Model object to add attributes for the view.
+     * @return The redirect view name after deleting the Admin account.
+     */
     @GetMapping("/admin/delete/{id}")
     public String deleteAdmin(@PathVariable("id") long id, Model model) {
         Admin admin = (Admin) accountRepository.findById(id);
